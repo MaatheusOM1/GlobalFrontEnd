c6cf9a0b2df83cedb26bcbacb7b9c461b72aa19d
MaatheusOM1 <matheus.macedo2005@gmail.com>
2023-11-23 17:39:05 -0300

    Home

---
+++ pages/home.js
@@ -0,0 +1,15 @@
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Texto from '../components/Texto'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Texto />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;

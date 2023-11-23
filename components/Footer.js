bd243984b82c2ea6158af41acef8363bb8ddc61e
MaatheusOM1 <matheus.macedo2005@gmail.com>
2023-11-23 17:26:44 -0300

    Component Footer

---
+++ components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer>
+      <h3>Equipe:</h3>
+      <ul>
+        <li>Breno Giacoppini Câmara - RM98695, Turma TDSY, Python</li>
+        <li>Felipe Batista Gregório - RM99985, Turma TDSY, Software Design</li>
+        <li>Guilherme Rodrigues Araujo - RM97617, Turma TDSW, IA e Chatbot</li>
+        <li>Matheus Oliveira Macedo - RM551155, Turma TDSY, FrontEnd</li>
+        <li>Victor Freitas Silva - RM99982, Turma TDSY, Java</li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;

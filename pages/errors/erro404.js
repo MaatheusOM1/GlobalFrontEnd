801f7d0b5fc226847e3747d0c4be70cc484ffae5
MaatheusOM1 <matheus.macedo2005@gmail.com>
2023-11-23 17:24:21 -0300

    Erro 404

---
+++ pages/errors/erro404.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return (
+      <div>
+        <h1>404 - Página não encontrada</h1>
+        <p>Desculpe, a página que você está procurando não existe ou não foi encontrada.</p>
+      </div>
+    );
+  };
+  
+  export default NotFound;
+  

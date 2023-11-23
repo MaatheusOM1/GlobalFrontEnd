1afd8b257447d550829d157d1ee7570ec74a7aa5
MaatheusOM1 <matheus.macedo2005@gmail.com>
2023-11-23 17:24:39 -0300

    Erro 500

---
+++ pages/errors/erro500.js
@@ -0,0 +1,11 @@
+const ServerError = () => {
+    return (
+      <div>
+        <h1>500 - Erro interno do servidor</h1>
+        <p>Desculpe, algo deu errado no servidor.</p>
+      </div>
+    );
+  };
+  
+  export default ServerError;
+  

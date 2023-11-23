c583e40af68f6a9b83ffb34a19486c5dfd01b337
MaatheusOM1 <matheus.macedo2005@gmail.com>
2023-11-23 17:23:33 -0300

    Erro 400

---
+++ pages/errors/erro400.js
@@ -0,0 +1,11 @@
+const Unauthorized = () => {
+    return (
+      <div>
+        <h1>400 - Solicitação Inválida</h1>
+        <p>Servidor não pode processar a requisição devido a alguma coisa que foi entendida como um erro do usuário.</p>
+      </div>
+    );
+  };
+  
+  export default Unauthorized;
+  

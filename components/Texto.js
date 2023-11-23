05cb947e4af01a987b884fc70ac53412ccab671f
MaatheusOM1 <matheus.macedo2005@gmail.com>
2023-11-23 17:27:18 -0300

    Component Texto

---
+++ components/Texto.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Texto.scss';
+
+const Texto = () => {
+  return (
+    <div>
+      <h2>Bem vindo ao seu controle de natalidade e maternidade!</h2>
+      <h3>Aqui você pode monitorar a si mesmo e a sua criança.</h3>
+      <h4>Monitore suas alimentações, vacinações, exercícios e muito mais!</h4>
+    </div>
+  );
+};
+
+export default Texto;

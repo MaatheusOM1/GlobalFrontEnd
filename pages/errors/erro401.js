fa2c906b90b2fd42b9ecf90c352e2457cfd86984
MaatheusOM1 <matheus.macedo2005@gmail.com>
2023-11-23 17:24:05 -0300

    Erro 401

---
+++ pages/errors/erro401.js
@@ -0,0 +1,11 @@
+const Unauthorized = () => {
+    return (
+      <div>
+        <h1>401 - Não autorizado</h1>
+        <p>Você não tem permissão para acessar esta página, verifique suas credenciais e tente novamente.</p>
+      </div>
+    );
+  };
+  
+  export default Unauthorized;
+  
